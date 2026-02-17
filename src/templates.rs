ceb947ef74b88602bbaadead3974794ed73b3bb0
Xavier Bestel <xav@bes.tel>
2026-02-17 06:59:09 +0100

    google cloud verif

--- src/templates.rs
+++ src/templates.rs
@@ -220,6 +220,7 @@ fn page(
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="S04nKUrv5gsWl0VqBBdd9Q6zS7rxLWHJLc2aFftaD4E" />
     <title>{title}</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.0/css/fontawesome.min.css">
